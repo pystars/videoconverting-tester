b4e71e892825b8a5cc5871a4a8ba1aef4aa61f11
Василий Большаков <va.bolshakov@gmail.com>
2011-11-22 01:59:37 +0400

    celery added

--- apps/video/models.py
+++ apps/video/models.py
@@ -1,27 +1,40 @@
 # -*- coding: utf-8 -*-
+import os
+
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 from django.utils.translation import ugettext as _
-import os
 
 from apps.options.models import AudioOptions, VideoOptions
-#from django.conf import settings
-#from django.template.defaultfilters import slugify
 from apps.utils.common import nullable
-from apps.video.stupff import convert_file
+from apps.video.stupff import FFmpegFile
+from apps.video.tasks import ConvertVideoTask
 
 
-class Original(models.Model):
+
+class VideoFile(models.Model):
+    class Meta:
+        abstract = True
+
+    def file_path(self):
+        return self.file.path
+
+    def ffmpeg_file(self, exists=True):
+        return FFmpegFile(self.file_path(), exists)
+
+
+class Original(VideoFile):
     title = models.CharField(_('Title'), max_length=40)
-    file = models.FileField('Файл для загрузки', upload_to='originals/')
+    file = models.FileField(upload_to='originals/', **nullable)
     enabled = models.BooleanField(verbose_name=_('Enabled'), default=True)
 
     def __unicode__(self):
         return u'{0}'.format(self.title)
 
 
-class Converted(models.Model):
+
+class Converted(VideoFile):
     PENDING = 0
     PROCESSING = 1
     READY = 2
@@ -32,13 +45,14 @@ class Converted(models.Model):
         (READY, _('Ready')),
         (ERROR, _('Error')),
     )
+
     original = models.ForeignKey(Original)
+    file = models.FileField(upload_to='converted/', editable=False, **nullable)
     audio_options = models.ForeignKey(AudioOptions)
     video_options = models.ForeignKey(VideoOptions)
-    file = models.FileField(upload_to='converted/', **nullable)
     splash_image = models.ImageField(_('Splash Image'),
                         upload_to='images/', max_length=200, **nullable)
-    locked = models.BooleanField(('Locked'), default=True)
+    locked = models.BooleanField(('Locked'), default=True, editable=False)
     state = models.IntegerField(_('State'), default=PENDING, choices=STATE)
 
     def __unicode__(self):
@@ -46,7 +60,7 @@ class Converted(models.Model):
             self.original.title, self.video_options, self.audio_options
         )
 
-    def generate_file_path(self):
+    def file_path(self):
         return self.file.storage.path(self.file.field.generate_filename(
             self,
             str(self.original.id) +
@@ -58,17 +72,8 @@ class Converted(models.Model):
 @receiver(post_save, sender=Converted)
 def convert_from_original(sender, **kwargs):
     converted = kwargs['instance']
-    file_path = converted.generate_file_path()
-    print file_path
-    if os.path.exists(file_path):
+    print converted.file_path()
+    if os.path.exists(converted.file_path()):
         return
-    convert_file(
-        converted.original.file.path,
-        file_path,
-        audio_options = converted.audio_options,
-        video_options = converted.video_options
-    )
-    converted.state = sender.READY
-    converted.locked = False
-    converted.save()
-    print 'converted'
+    ConvertVideoTask.delay(converted)
+
--- apps/video/tasks.py
+++ apps/video/tasks.py
@@ -1,12 +1,39 @@
+import subprocess
 from celery.task import Task
-from apps.video.models import Converted
 
 
 class ConvertVideoTask(Task):
-    """
-    Converts the Video and creates the related files.
-    """
+    extra_ffmpeg_args = []
+
+    def get_commandline(self):
+        return (
+            ['ffmpeg', '-v', '10'] +
+            self.extra_ffmpeg_args +
+            ['-i', self.original_file.filename] +
+            list(self.audio_options.as_commandline()) +
+            list(self.video_options.as_commandline()) +
+            [self.result_file.filename]
+        )
+    
     def run(self, converted, **kwargs):
-        converted.state = Converted.PROCESSING
+        """
+        Converts the Video and creates the related files.
+        """
+        logger = self.get_logger(**kwargs)
+        logger.info("Starting Video Post conversion: %s" % converted)
+
+        self.original_file = converted.original.ffmpeg_file()
+        self.result_file = converted.ffmpeg_file(False)
+        self.audio_options = converted.audio_options
+        self.video_options = converted.video_options
+
+        converted.state = converted.PROCESSING
+        converted.save()
+        process = subprocess.call(self.get_commandline(), shell=False)
+        if process:
+            converted.state = converted.ERROR
+        else:
+            converted.state = converted.READY
+            converted.locked = False
         converted.save()
         return "Ready"
